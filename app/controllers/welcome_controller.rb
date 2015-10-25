01aa8065c47fe7799e0663f3668f3968343cff28
Nick Brennan <nick_p_b@yahoo.com>
2015-10-25 11:54:09 -0700

    added auth action to welcome controller and basic routing

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,40 @@ class WelcomeController < ApplicationController
 
   def home
   end
+
+	def strava_auth
+	    code = params["code"];
+	    response_temp = HTTParty.post("https://www.strava.com/oauth/token", body: {
+	    client_id: ENV['STRAVA_ID'],
+	    client_secret: ENV['STRAVA_KEY'] ,
+	    code: code
+	    }.to_json,
+	    headers: {'Content-Type'=>'application/json'})
+
+	    id=response_temp.parsed_response["athlete"]["id"]
+	    user = User.find_by strava_id: id
+
+	    if !user
+		    user = User.new
+		    user.token = response_temp.parsed_response["access_token"]
+		    user.strava_id = response_temp.parsed_response["athlete"]["id"]
+		    user.firstname = response_temp.parsed_response["athlete"]["firstname"]
+		    user.lastname = response_temp.parsed_response["athlete"]["lastname"]
+		    user.email = response_temp.parsed_response["athlete"]["email"]
+		    user.city = response_temp.parsed_response["athlete"]["city"]
+		    user.state = response_temp.parsed_response["athlete"]["state"]
+		    user.profile_img = response_temp.parsed_response["athlete"]["profile"]       
+	    end
+
+	    user.firstname = response_temp.parsed_response["athlete"]["firstname"]
+	    user.lastname = response_temp.parsed_response["athlete"]["lastname"]
+	    user.email = response_temp.parsed_response["athlete"]["email"]
+	    user.city = response_temp.parsed_response["athlete"]["city"]
+	    user.state = response_temp.parsed_response["athlete"]["state"]
+	    user.profile_img = response_temp.parsed_response["athlete"]["profile"]
+	    user.save
+
+	    session[:user_id] = user.id    
+	    redirect_to '/home'
+	end
 end
